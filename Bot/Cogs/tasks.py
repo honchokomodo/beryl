1c1545684edd54a678496d95cc493ca71318ab3d
No767 <73260931+No767@users.noreply.github.com>
2022-07-13 00:40:43 -0700

    Create Asyncio Task for CeleCheck

--- Bot/Cogs/tasks.py
+++ Bot/Cogs/tasks.py
@@ -3,7 +3,7 @@ import datetime
 import logging
 
 from beryl_events_utils import BerylEventsUtils
-from discord.ext import commands, tasks
+from discord.ext import commands
 
 berylUtils = BerylEventsUtils()
 
@@ -19,32 +19,36 @@ class tasksCheckerUtils:
         self.self = self
 
     async def celeCheck(self):
-        projectedValues = await berylUtils.findProjection()
-        for res in projectedValues:
-            dictRes = dict(res)
-            today = datetime.date.today()
-            eventDate = datetime.date(dictRes["year"], dictRes["month"], dictRes["day"])
-            if eventDate is not today:
-                logging.info(f"Date: {today} | There are no events today :(")
-            elif eventDate > today:
-                logging.info("Setting event as passed")
-                await berylUtils.setEventPassed()
-            else:
-                logging.info(f"Date: {today} | There are events today! :3")
+        while True:
+            await asyncio.sleep(3600)
+            projectedValues = await berylUtils.findProjection()
+            for res in projectedValues:
+                dictRes = dict(res)
+                today = datetime.date.today()
+                eventDate = datetime.date(
+                    dictRes["year"], dictRes["month"], dictRes["day"]
+                )
+                if eventDate is not today:
+                    logging.info(f"Date: {today} | There are no events today :(")
+                elif eventDate > today:
+                    logging.info("Setting event as passed")
+                    await berylUtils.setEventPassed()
+                else:
+                    logging.info(f"Date: {today} | There are events today! :3")
 
 
 class tasksUtils(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.checkEvents.start()
 
-    # Not sure if this will work or not...
-    @tasks.loop()
-    async def checkEvents(self):
+    @commands.Cog.listener()
+    async def on_ready(self):
         celeCheckerUtils = tasksCheckerUtils()
-        await asyncio.sleep(5)
-        logging.info("Checking events...")
-        await celeCheckerUtils.celeCheck()
+        task = asyncio.create_task(celeCheckerUtils.celeCheck(), name="CeleCheck")
+        background_tasks = set()
+        background_tasks.add(await task)
+        task.add_done_callback(background_tasks.discard)
+        logging.info("Started Event Checker - Events will be checked every hour")
 
 
 def setup(bot):
